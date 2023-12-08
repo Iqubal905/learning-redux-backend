7f9d5d6c200988580760ef07451799d18148ec9f
ocircleo <salmanhossain11222626@gmail.com>
2023-12-08 18:25:49 +0600

    project deployed

--- index.js
+++ index.js
@@ -10,28 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("views"));
 //db and mongoose connect
-async function connectToDB() {
-  console.log("trying to connect");
-  try {
-    mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@ocircleo.zgezjlp.mongodb.net/redux?retryWrites=true&w=majority`,
-    );
-    console.log("connected to mongodb");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@ocircleo.zgezjlp.mongodb.net/redux?retryWrites=true&w=majority`
+);
 //routes and others
-
 app.use("/", routes);
 app.use((req, res) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "./views/public/pages/error.html"));
+  res.sendFile(path.join(__dirname, "./views/public/pages/error.html"));
 });
 
 app.listen(port, async () => {
-  connectToDB();
   console.log(`server is running at http://localhost:${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,20 @@
 const express = require("express");
-
 const router = express.Router();
 const path = require("path");
 const { getRoutes } = require("./getRoutes");
 const { putRoutes } = require("./putRoutes");
 const { postRoutes } = require("./postRoutes");
+// /route
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/public/pages/index.html"));
+});
 //api routes
 router.use("/api/get", getRoutes);
 router.use("/api/post", postRoutes);
 router.use("/api/put", putRoutes);
+
 //html file routes
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/public/index.html"));
-});
+
 router.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/public/pages/about.html"));
 });
